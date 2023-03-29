9a8e620a1376684558b1bea560f88367f5fe745f
scelsic2 <scelsic2@gmail.com>
2023-03-29 09:13:34 -0400

    readme generates draft, still awaiting details such as badges

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
+console.log("Respond to the prompts to generate your README.md.")
 // TODO: Create an array of questions for user input
 // const questions = [];
 
 const readmePrompts = [
-    {type: "input",
-    name: "projectTitle",
+    {name: "projectTitle",
     message: "Enter project title:",
     default: "Project Title"},
     
-    {type: "input",
-    name: "description",
+    {name: "description",
     message: "Enter a description:",
     default: "Description"},
 
-    {type: "input",
-    name: "installation",
+    {name: "installation",
     message: "Installation:",
     default: ""},
 
-    {type: "input",
-    name: "usage",
+    {name: "usage",
     message: "Usage:",
     default: ""},
 
@@ -31,89 +30,43 @@ const readmePrompts = [
     choices: ["MIT", "Other", "GPLv2", "Apache", "GPLv3", "BSD 3-clause", "None"],
     validate:(value) => {if (value){return true}else {return "Please select a license."}}},
 
-    {type: "input",
-    name: "contributing",
+    {name: "contributing",
     message: "Contributing:",
     default: ""},
 
-    {type: "input",
-    name: "tests",
+    {name: "tests",
     message: "Tests:",
     default: ""},
 
-    {type: "input",
-    name: "questions",
-    message: "Questions:",
-    default: ""},
-
-    {type: "input",
-    name: "github",
+    {name: "github",
     message: "Github Username:",
     validate:(value) => {if (value){return true}else {return "You must enter a Github username to continue."}}},
 
-    {type: "input",name: "email",
+    {name: "email",
     message: "Email:",
     validate:(value) => {if (value){return true}else {return "You must enter an email address to continue."}}},
 ]
 
-inquirer.prompt(readmePrompts)
-    .then(({
-        projectTitle,
-        description,
-        installation,
-        usage,
-        license,
-        contributing,
-        tests,
-        questions,
-        github,
-        email
-    }) => {
-    const template = `# ${projectTitle}
-    ## [Description]
-    ${description}
-
-    ## [Table_of_Contents](#tableOfContents)
-    * [Installation](#installation)
-    * (Usage)(#usage)
-    * (License)(#license)
-    * (Contributing)(#contributing)
-    * (Tests)(#tests)
-    * (Questions)(#questions)
-    * (Github)(#github)
-    * (Email)(#email)
-   
-    ## Installation
-    ${installation}
-
-    ## Usage
-    ${usage}
-
-    ## License
-    ${license}
-
-    ## Contributing
-    ${contributing}
-
-    ## Tests
-    ${tests}
-
-    ## Questions
-    ${questions}
-
-    ## Contact
-    * Github
-    ${github}
-    * Email
-    ${email}`
-
-    })
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Your README file has been generated.")
+
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(readmePrompts)
+    .then(function(userInput){
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
